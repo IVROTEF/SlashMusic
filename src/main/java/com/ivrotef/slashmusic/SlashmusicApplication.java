c42dcb18d27f5f1917042fdc8a8a454b1e4f2da2
RodrigoVelazquez99 <rodrigo_velazquez99@ciencias.unam.mx>
2020-04-28 18:37:37 -0500

    estructura inicial

---
+++ src/main/java/com/ivrotef/slashmusic/SlashmusicApplication.java
@@ -0,0 +1,13 @@
+package com.ivrotef.slashmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlashmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlashmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivrotef/slashmusic/SlashmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivrotef.slashmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlashmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
